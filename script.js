c6d53b1953d0b7b1bdbbe56a4602f80a78309ccb
okaka favour <okakafavour81@gmail.com>
2025-06-16 00:39:11 +0100

    first commit

--- script.js
+++ script.js
@@ -6,17 +6,41 @@ var y = document.getElementById("register");
 function login (){
     x.style.left = "4px";
     y.style.right =  "-520px";
-    a.className += "white-btn";
-    b.className = "btn";
-    x.style.opacity = 1;
-    y.style.opacity = 0;
 }
 
 function register (){
     x.style.left = "-510px";
     y.style.right = "5px";
-    a.className = "btn";
-    b.className += "white-btn";
-    x.style.opacity = 0;
-    y.style.opcacity = 1;
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    const registerBtn = document.getElementById("registerSubmitBtn");
+
+    registerBtn.addEventListener("click", function(event){
+        event.preventDefault();
+
+        const fullName = document.getElementById("fullName").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const phoneNumber = document.getElementById("phoneNumber").value;
+
+        const requestData = {fullName, email, password, phoneNumber};
+
+        fetch("http://localhost:9090/api/libraryMember/register", {
+            method : "POST",
+            headers:{
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(requestData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                alert("Registered successfully!");
+                console.log(data);
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                alert("Registration failed.");
+            });
+    });
+})
